dbc464ac8fd94cd57234d48af04ca23313cbdce2
benedekaibas <kaibas.benedek02@gmail.com>
2025-02-23 01:17:50 -0500

    started the the implementation of the algorithms, but compiler error needs to be fixed

--- src/code/sorting.cpp
+++ src/code/sorting.cpp
@@ -4,5 +4,41 @@
 #include <thread>
 #include <chrono>
 
-using namespace std;
+//using namespace std;
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+const int NUM_BARS = 50;
+
+std::vector<int> array(NUM_BARS);
+
+void generate_array(){
+    for (int i = 0; i < NUM_BARS; i++){
+        array[i] = rand() % HEIGHT;
+    }
+}
+
+void display_array(sf::RenderWindow& window){
+    window.clear();
+    float bar_width = WIDTH / float(NUM_BARS);
+
+    for (int i = 0; i < NUM_BARS; i++){
+        sf::RectangleShape bar(sf::Vector2f(bar_width - 2, array[i]));
+        bar.setPosition(i * bar_width, HEIGHT - array[i]);
+        bar.setFillColor(sf::Color::White);
+        window.draw(bar);
+    }
+    window.display();
+}
+
+void bubble_sort(sf::RenderWindow& window){
+
+}
+
+int main(){
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Sorting Visualizer in C++");
+    generate_array();
+    display_array(window);
+
+    return 0;
+}
